42e3b601573b46ea72e498a6e4d5edf7c5254a56
sumudu hewage <sumudu.hewagee@gmail.com>
2019-03-11 14:13:03 +0530

    firstCommit

---
+++ wisdmlabs/todolist/src/TodolistServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace WisdmLabs\Todolist;
+
+use Illuminate\Support\ServiceProvider;
+
+class TodolistServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('WisdmLabs\Todolist\TaskController');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+        $this->loadViewsFrom(__DIR__.'/views', 'todolist');
+        $this->publishes([
+            __DIR__.'/views' => base_path('resources/views/wisdmlabs/todolist'),
+        ]);
+    }
+}
